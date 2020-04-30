7b6e5e1d63309af2f23bcba4f1122f9b37bd0585
ianulanday <ianulanday@email.arizona.edu>
2020-04-30 11:23:46 -0700

    gathering updates

--- odrive_hover_config.py
+++ odrive_hover_config.py
@@ -1,3 +1,11 @@
+import odrive
+from odrive.utils import dump_errors
+from odrive.enums import *
+import time
+import math
+
+odrv0 = odrive.find_any()
+
 # configure pole pairs for standard hoverboard hub motor 
 odrv0.axis0.motor.config.pole_pairs = 15
 
@@ -25,9 +33,27 @@ odrv0.reboot()
 # Make sure the motor is free to move, then activate the motor calibration.
 odrv0.axis0.requested_state = AXIS_STATE_MOTOR_CALIBRATION
 
+'''TODO: check these values are set correctly`
+  error = 0x0000 (int)
+  phase_inductance = 0.00033594953129068017 (float)
+  phase_resistance = 0.1793474406003952 (float)
+'''
+
+# let ya boi breathe
+odrv0.axis0.motor.config.pre_calibrated = True
+
+odrv0.save_configuration()
+odrv0.reboot()
+
+odrv0.axis0.requested_state = AXIS_STATE_ENCODER_OFFSET_CALIBRATION
+time.sleep(5)
+
 # Set velocity to some value
+#   RPM * 2*pi/60 * polepairs = vel rad/s electrical
 odrv0.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-odrv0.axis0.controller.vel_setpoint = some_velocity_value
+odrv0.axis0.controller.vel_setpoint = 50 * 2*math.pi/60 * 15
+
+time.sleep(5)
 
 # Return to idle
 odrv0.axis0.controller.vel_setpoint = 0
